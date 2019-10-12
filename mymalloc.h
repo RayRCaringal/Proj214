63eb28d2a44e35a2a8a90b778100b1b71a814131
RayRCaringal <rayrcaringal@gmail.com>
2019-10-12 18:03:59 -0400

    Added functions

--- mymalloc.h
+++ mymalloc.h
@@ -1,6 +1,21 @@
 #ifndef MYMALLOC_H
 #define MYMALLOC_H
 
+static char memory[4096];
+
+typedef struct block Block;
+  struct block{
+  int size;
+  bool free;
+  Block * next;  
+};
+
+void intialize(Block *metadata);
+void split(Block *metadata, size_t size);
+void merge(Block * metadata);
+void* myMalloc(Block *metadata,size_t size);
+void myFree(Block * metadata);
+
 
 #define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
 #define free( x ) myfree( x, __FILE__, __LINE__ )
